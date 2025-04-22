372846167ea04769fcfc6ce8b24fa867bbb4dcd3
Adit122022 <adity202212@gmail.com>
2025-04-22 10:52:47 +0530

    --->setting the server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const app = require('./src/app');
-// const connect = require('./src/db/db');
-
-// connect();
+import app from './src/app.js'
+import config from "./src/config/config.js";
+const PORT =config.PORT
 
 app.listen(PORT, () => {
     console.log('Server is running on port' , PORT);  // Server is listening on port 3000
